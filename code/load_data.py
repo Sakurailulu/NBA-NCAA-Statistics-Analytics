ab217519c3d08c01664d5afbd1871885a9d69023
Michelle Zhou <michellezhou71@gmail.com>
2020-05-05 17:19:06 -0400

    Corrected dataset location, redownloaded datasets

--- code/load_data.py
+++ code/load_data.py
@@ -1,13 +1,22 @@
 import psycopg2
+import psycopg2.extras
+import csv
 
 connection_string = "host='localhost' dbname='dbms_final_project' user='dbms_project_user' password='dbms_password'"
-
-# TODO add your code here (or in other files, at your discretion) to load the data
+conn = psycopg2.connect(connection_string, cursor_factory=psycopg2.extras.DictCursor)
+cursor = conn.cursor()
 
 
 def main():
-    # TODO invoke your code to load the data into the database
+    # Creating schema
+    print("Creating schema...")
+    cursor.execute(open("schema.sql", "r").read())
+
+
     print("Loading data")
+    
+
+
 
 
 if __name__ == "__main__":
