65f777fbff3eeade8a426fcf3355d63db333376a
FelicToken <47868673+FelicToken@users.noreply.github.com>
2020-05-06 19:40:11 -0400

    Update queries.py

--- code/queries.py
+++ code/queries.py
@@ -18,8 +18,14 @@ query=" SELECT AVG(height),MAX(height),MIN(height) from ncaa_stats,ncaa_players 
 #top 10 players with highest # of fg, three pointers, free throws, rebound, assists, etc
 query="SELECT player_name FROM ncaa_stats ORDER BY fg_attempts DESC LIMIT 10"
 query="SELECTplayer_name FROM ncaa_stats ORDER BY tp_attempts DESC LIMIT 10"
-query="SELECT player_name FROM ncaa_stats ORDER BY points DESC LIMIT 10"
 query="SELECT player_name FROM ncaa_stats ORDER BY games_played DESC LIMIT 10"
+query="SELECTplayer_name FROM ncaa_stats ORDER BY three_pointers DESC LIMIT 10"
+query="SELECT player_name FROM ncaa_stats ORDER BY rebounds DESC LIMIT 10"
+query="SELECT player_name FROM ncaa_stats ORDER BY assists DESC LIMIT 10"
+query="SELECTplayer_name FROM ncaa_stats ORDER BY  blocks DESC LIMIT 10"
+query="SELECT player_name FROM ncaa_stats ORDER BY  steals DESC LIMIT 10"
+query="SELECT player_name FROM ncaa_stats ORDER BY pts DESC LIMIT 10"
+query="SELECT player_name FROM ncaa_stats ORDER BY turnovers DESC LIMIT 10"
 
 #Top 10 players with highest % of attribute
 query="SELECT player_name FROM ncaa_stats ORDER BY fg_percentage DESC LIMIT 10"
@@ -28,7 +34,7 @@ query="SELECT player_name FROM ncaa_stats ORDER BY rb_percentage DESC LIMIT 10"
 query="SELECT player_name FROM ncaa_stats ORDER BY ass_percentage DESC LIMIT 10"
 query="SELECTplayer_name FROM ncaa_stats ORDER BY st_percentage DESC LIMIT 10"
 query="SELECT player_name FROM ncaa_stats ORDER BY bl_percentage DESC LIMIT 10"
-query="SELECT player_name FROM ncaa_stats ORDER BY turnover_percentage DESC LIMIT 10"
+query="SELECT player_name FROM ncaa_stats ORDER BY pts_pergame DESC LIMIT 10"
 
 
 
@@ -41,7 +47,7 @@ query="SELECT player_name FROM nba_stats ORDER BY fg_attempts DESC LIMIT 10"
 query="SELECTplayer_name FROM nba_stats ORDER BY tp_attempts DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY points DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY games_played DESC LIMIT 10"
-
+query="SELECT player_name FROM nba_stats ORDER BY three_pointers DESC LIMIT 10"
 #Top 10 players with highest % of attribute
 query="SELECT player_name FROM nba_stats ORDER BY fg_percentage DESC LIMIT 10"
 query="SELECTplayer_name FROM nba_stats ORDER BY tp_percentage DESC LIMIT 10"
