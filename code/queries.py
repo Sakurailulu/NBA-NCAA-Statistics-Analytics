a946afd632c943f77368cfe106baf1830ee803fa
Sakurailulu <34151383+Sakurailulu@users.noreply.github.com>
2020-05-07 00:12:59 -0400

    Update queries.py

--- code/queries.py
+++ code/queries.py
@@ -44,7 +44,7 @@ query=" SELECT AVG(height),MAX(height),MIN(height)from nba_stats,nba_players WHE
 
 #top 10 players with highest # of fg, three pointers, free throws, rebound, assists, etc
 query="SELECT player_name FROM nba_stats ORDER BY fg_attempts DESC LIMIT 10"
-query="SELECTplayer_name FROM nba_stats ORDER BY tp_attempts DESC LIMIT 10"
+query="SELECT player_name FROM nba_stats ORDER BY tp_attempts DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY points DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY games_played DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY three_pointers DESC LIMIT 10"
@@ -53,7 +53,7 @@ query="SELECT player_name FROM nba_stats ORDER BY fg_percentage DESC LIMIT 10"
 query="SELECTplayer_name FROM nba_stats ORDER BY tp_percentage DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY rb_percentage DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY ass_percentage DESC LIMIT 10"
-query="SELECTplayer_name FROM nba_stats ORDER BY st_percentage DESC LIMIT 10"
+query="SELECT player_name FROM nba_stats ORDER BY st_percentage DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY bl_percentage DESC LIMIT 10"
 query="SELECT player_name FROM nba_stats ORDER BY turnover_percentage DESC LIMIT 10"
 
